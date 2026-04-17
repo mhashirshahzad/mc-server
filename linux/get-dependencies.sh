2c51067e0cc4e60fc55ffa805a9064491ad31c3c
Bongo Poyo <bongopoyo@proton.me>
2026-04-17 12:42:52 +0500

    fixing app image still

---
+++ linux/get-dependencies.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -eu
+
+# get-dependencies.sh - For Arch Linux container
+
+# Initialize pacman keyring (needed in fresh containers)
+pacman-key --init
+pacman-key --populate archlinux
+
+# Update and install
+pacman -Sy --noconfirm \
+    python \
+    python-gobject \
+    python-requests \
+    python-appdirs \
+    gtk4 \
+    libadwaita \
+    gobject-introspection \
+    glib2 \
+    git \
+    base-devel \
+    wget \
+    file \
+    binutils
+
+# For AppImage tools
+pacman -S --noconfirm \
+    fuse2 \
+    fuse3
+
+echo "All dependencies installed!"
